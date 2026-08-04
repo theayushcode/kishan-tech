8fda2ad8e109b71104cdc81c170f3050ab427d41
Ayush Narayan <narayan.ayush0701@gmail.com>
2026-08-04 08:49:49 +0530

    Removed hardcoded secrets from server.js

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,15 @@ app.use(cors());
 app.use(express.json());
 
 // ==========================================
+/// ==========================================
 // 1. MySQL Connection Setup (Aiven Cloud Database)
 // ==========================================
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'mysql-1347603b-kishan-tech.f.aivencloud.com',
-    user: process.env.DB_USER || 'avnadmin',
-    password: process.env.DB_PASSWORD || 'AVNS_my4Rx5J1AhtihxOmpY1',
-    database: process.env.DB_NAME || 'defaultdb',
-    port: process.env.DB_PORT || 25152,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false // Aiven Cloud DB ke liye SSL zaroori hai
     }
